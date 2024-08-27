bc8c39f33d672411350804ba282eff41e7698437
Drew <drew@drewgriffin.com>
2024-08-27 13:37:50 -0400

    update to alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -24,7 +24,16 @@ return
       [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
       [[                                                                       ]],
       [[                                                                       ]],
-      [[                                                                       ]],
+	  [[Keymap]                                           ]],
+      [[                                                                                         ]],
+      [[    Find File:  Spacebar + ff            ||       File Word: Spacebar + fw               ]],
+      [[   Create Tab:  Spacebar + t             ||       Close Tab: Spacebar + x                ]],
+      [[     Next Tab:  Spacebar + j             ||        Prev Tab: Spacebar + k                ]],
+      [[  Next Buffer:  Spacebar + Tab           ||     Prev Buffer: Spacebar + Shift+Tab        ]],
+      [[  Split Horiz:  Spacebar + s             ||      Split Vert: Spacebar + v                ]],
+      [[  Resize Left:  Spacebar + Ctrl + Left   ||    Resize Right: Spacebar + Ctrl + Right     ]],
+      [[                                                                                         ]],
+      [[                                  Files: Spacebar + e                                    ]],
     }
 
     alpha.setup(dashboard.opts)
