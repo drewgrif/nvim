3f2eb5d50fefce3b55f93eb5b6220a8dbae9689a
drewgrif <jag@justaguylinux.com>
2025-03-28 11:04:28 -0400

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,34 @@
-return
-{
+return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate", -- auto-update when installing via Lazy
+  event = { "BufReadPost", "BufNewFile" }, -- lazy-load when files are opened
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rasi", "bash", "go" },
+    -- Avoid "Press ENTER" issues
+    vim.opt.shortmess:append("I")
+    vim.opt.confirm = false
+    vim.opt.more = false
 
-      auto_install = true,
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c", "lua", "vim", "vimdoc", "query",
+        "python", "rasi", "bash", "cpp", "json", "yaml", "css",
+        "markdown", "markdown_inline",
+      },
+      auto_install = true, -- installs missing parsers automatically
 
       highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false,
       },
+    })
+
+    -- Optional: one-time parser sync on startup (avoids manual :TSInstall)
+    vim.api.nvim_create_autocmd("BufEnter", {
+      once = true,
+      callback = function()
+        local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+        ts_update()
+      end,
     })
   end,
 }
